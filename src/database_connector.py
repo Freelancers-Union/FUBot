5fd6ce65cd0be5389e5dbc16d980c7ee5c6556a0
Lloyd P <71779816+Normandabald@users.noreply.github.com>
2022-08-18 18:01:47 +0100

    Feature/connect ps2 discord (#33)
    * added mongodb to docker images
    
    * Added changes related to moving .env file to repo root
    
    * Updated docs for mongodb
    
    * Added basic framework to start connecting Discord and PS2 names
    
    * user initated PS2/Discord link concept working
    
    * Added support for alt accounts
    
    * wip refactor to make prettier
    
    * mega refactor
    
    Co-authored-by: karlis-ozo <52657221+karlis-ozo@users.noreply.github.com>

---
+++ src/database_connector.py
@@ -0,0 +1,44 @@
+import os
+import logging
+import pymongo
+
+
+class Database(object):
+    URI = (
+        "mongodb://"
+        + str(os.getenv("MONGO_USERNAME"))
+        + ":"
+        + str(os.getenv("MONGO_PASSWORD"))
+        + "@localhost:27017/"
+    )
+    DATABASE = None
+
+    @staticmethod
+    def initialize():
+        try:
+            client = pymongo.MongoClient(Database.URI, serverSelectionTimeoutMS=5000)
+        except Exception as e:
+            raise e
+        else:
+            Database.DATABASE = client["freelancers_union"]
+            logging.info("Database Initialized")
+
+    @staticmethod
+    def insert_one(collection, data):
+        Database.DATABASE[collection].insert_one(data)
+
+    @staticmethod
+    def insert_many(collection, data):
+        Database.DATABASE[collection].insert_many(data)
+
+    @staticmethod
+    def update_one(collection, query, data):
+        Database.DATABASE[collection].update_one(query, data)
+
+    @staticmethod
+    def find(collection, query):
+        return Database.DATABASE[collection].find(query)
+
+    @staticmethod
+    def find_one(collection, query):
+        return Database.DATABASE[collection].find_one(query)
