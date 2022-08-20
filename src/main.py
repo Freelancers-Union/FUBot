d9181a78e59bd742cbf213f2452c10bac610e4b2
Normandabald <lpassingham@hotmail.co.uk>
2022-08-20 23:09:58 +0100

    linted main.py and added new ps2_login checks

--- src/main.py
+++ src/main.py
@@ -17,13 +17,12 @@ from ps2_db import *
 import emoji
 import re
 
-from dotenv import load_dotenv
 
-load_dotenv()
-
-
-logging.basicConfig(level=logging.os.getenv('LOGLEVEL'), format='%(asctime)s %(funcName)s: %(message)s ',
-                    datefmt='%m/%d/%Y %I:%M:%S %p')
+logging.basicConfig(
+    level=logging.os.getenv("LOGLEVEL"),
+    format="%(asctime)s %(funcName)s: %(message)s ",
+    datefmt="%m/%d/%Y %I:%M:%S %p",
+)
 
 # Discord Intents
 
@@ -34,30 +33,23 @@ intents.guilds = True
 
 # Initialize the bot
 
-discordClientToken = os.getenv('DISCORDTOKEN')
+discordClientToken = os.getenv("DISCORDTOKEN")
 Botdescription = "The serious bot for the casual Discord."
 
-# if os.getenv('TEST_GUILD_ID') is not None:
-#     bot = commands.Bot(
-#         command_prefix=commands.when_mentioned_or("?"),
-#         description=Botdescription,
-#         intents=intents,
-#         test_guilds=[int(os.getenv('TEST_GUILD_ID'))],
-#         sync_commands_debug=False
-#     )
-# else:
 bot = commands.Bot(
     command_prefix=commands.when_mentioned_or("?"),
     description=Botdescription,
     intents=intents,
-    sync_commands_debug=False
+    sync_commands_debug=False,
 )
 
 Database.initialize()
 
+
 @bot.event
 async def on_ready():
     await PlayerLogin()
+    await PlayerLogout()
     logging.info("Logged in as " + str(bot.user) + " (ID: " + str(bot.user.id) + ")")
     logging.info("FUBot is ready!")
 
@@ -70,8 +62,8 @@ async def add(ctx, left: int, right: int):
 
 @bot.slash_command()
 async def player_card(
-        inter: disnake.CommandInteraction,
-        character_name: str,
+    inter: disnake.CommandInteraction,
+    character_name: str,
 ):
     """
     Get character information for a given character
@@ -86,7 +78,9 @@ async def player_card(
         if player_card is not None:
             await inter.edit_original_message(" ", embed=player_card)
         else:
-            await inter.edit_original_message("Could not find character: " + str(character_name))
+            await inter.edit_original_message(
+                "Could not find character: " + str(character_name)
+            )
     except Exception as e:
         await inter.edit_original_message("Hmm, looks like something went wrong.")
         logging.exception(e)
@@ -94,9 +88,9 @@ async def player_card(
 
 @bot.slash_command()
 async def outfit(
-        inter: disnake.ApplicationCommandInteraction,
-        tag: str = 0,
-        name: str = 0,
+    inter: disnake.ApplicationCommandInteraction,
+    tag: str = None,
+    name: str = None,
 ):
     """
     Get Outfit information for a given outfit
@@ -112,21 +106,36 @@ async def outfit(
         if outfit_card is not None:
             await inter.edit_original_message("", embed=outfit_card)
         else:
-            await inter.edit_original_message("Could not find outfit: " + str(name) + str(tag))
+            await inter.edit_original_message(
+                "Could not find outfit: " + str(name) + str(tag)
+            )
     except Exception as e:
         await inter.edit_original_message("Hmm, looks like something went wrong.")
         logging.exception(e)
 
 
-EVENTS = ["Drill", "Casual", "FUAD", "FUAF", "FUBG", "FUEL", "FUGG", "Huntsmen", "ArmaOps"]
+EVENTS = [
+    "Drill",
+    "Casual",
+    "FUAD",
+    "FUAF",
+    "FUBG",
+    "FUEL",
+    "FUGG",
+    "Huntsmen",
+    "ArmaOps",
+]
+
+
 async def autocomplete_event(inter, string: str) -> List[str]:
     return [event for event in EVENTS if string.lower() in event.lower()]
 
+
 @bot.slash_command(dm_permission=False)
 async def announce_event(
-        inter: disnake.CommandInteraction,
-        event: str = commands.Param(autocomplete=autocomplete_event),
-        message_body: str = "Find us in game."
+    inter: disnake.CommandInteraction,
+    event: str = commands.Param(autocomplete=autocomplete_event),
+    message_body: str = "Find us in game.",
 ):
     """
     Post an event announcement to #ps2-announcements
@@ -147,22 +156,28 @@ async def announce_event(
 
 @bot.message_command(name="Add Reactions")
 @commands.default_member_permissions(manage_messages=True)
-async def vote(inter: disnake.interactions.application_command.ApplicationCommandInteraction,
-               message: disnake.Message):
+async def vote(
+    inter: disnake.interactions.application_command.ApplicationCommandInteraction,
+    message: disnake.Message,
+):
     emoji_list: list
 
     await inter.response.defer(ephemeral=True)
     # Permission checks
     # this is as a catch just in case the default_members_permissions fail
     if not message.channel.permissions_for(inter.author).manage_messages:
-        await inter.edit_original_message("Request denied.\n" +
-                                          "You don't have the permissions to remove unneeded reactions or spam in " +
-                                          message.channel.mention + "\nhttps://www.govloop.com/wp-content/uploads"
-                                                                    "/2015/02/data-star-trek-request-denied.gif"
-                                          )
+        await inter.edit_original_message(
+            "Request denied.\n"
+            + "You don't have the permissions to remove unneeded reactions or spam in "
+            + message.channel.mention
+            + "\nhttps://www.govloop.com/wp-content/uploads"
+            "/2015/02/data-star-trek-request-denied.gif"
+        )
         return
 
-    discord_emojis = list(set(re.compile(r"<:.*:[0-9]*>").findall(message.content)))  # some magic to delete duplicates
+    discord_emojis = list(
+        set(re.compile(r"<:.*:[0-9]*>").findall(message.content))
+    )  # some magic to delete duplicates
     emoji_list = emoji.distinct_emoji_list(message.content) + discord_emojis
 
     for item in emoji_list:
@@ -176,13 +191,14 @@ async def send_scheduled_message():
     Scheduled task to post new Discord members report.
     Cron: Every Friday at 1700 UTC
     """
-    
+
     weeklyNewMemberReport = new_discord_members.NewDiscordMembers(bot)
     try:
         for guild in bot.guilds:
             await weeklyNewMemberReport.post_member_report(guild)
     except Exception as e:
         logging.exception(e)
+
 
 bot.load_extension("commands.role_added")
 bot.load_extension("commands.new_discord_members")
