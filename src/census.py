9aca90a2824c3d3f30daf6d6982fcff85fb669b4
Normandabald <lpassingham@hotmail.co.uk>
2022-08-20 23:08:40 +0100

    census.py had poor arg validation

--- src/census.py
+++ src/census.py
@@ -5,7 +5,9 @@ from auraxium import ps2, Client
 from typing import Tuple
 
 
-async def get_character(char_name: str, client: Client) -> Tuple[ps2.Character, int, ps2.OutfitMember]:
+async def get_character(
+    char_name: str, client: Client
+) -> Tuple[ps2.Character, int, ps2.OutfitMember]:
     """
     Gets character, world it is in and its outfit
     Parameters
@@ -46,9 +48,9 @@ async def get_outfit(outfit_tag, outfit_name, client):
     outfit = None
     online_members = None
     try:
-        if outfit_tag != 0:
+        if outfit_tag is not None:
             outfit = await client.get(ps2.Outfit, alias_lower=outfit_tag.lower())
-        elif outfit_name != 0:
+        elif outfit_name is not None:
             outfit = await client.get(ps2.Outfit, name_lower=outfit_name.lower())
         if outfit is not None:
             online_members = await get_online_outfit(outfit.id)
@@ -74,12 +76,22 @@ async def get_online_outfit(outfit) -> int:
     """
     count = 0
     try:
-        url = "https://census.daybreakgames.com/"+str(os.getenv('CENSUS_TOKEN'))+"/get/ps2:v2/outfit?outfit_id=" + \
-              str(outfit) + "&c:resolve=member_character&c:join=characters_online_status%5Eon:members.character_id%5Eto:character_id%5Einject_at:character_online_status"
+        url = (
+            "https://census.daybreakgames.com/"
+            + str(os.getenv("CENSUS_TOKEN"))
+            + "/get/ps2:v2/outfit?outfit_id="
+            + str(outfit)
+            + "&c:resolve=member_character&c:join=characters_online_status%5Eon:members.character_id%5Eto:character_id%5Einject_at:character_online_status"
+        )
         response = requests.get(url)
-        member_count = len(response.json()['outfit_list'][0]['members'])
+        member_count = len(response.json()["outfit_list"][0]["members"])
         for i in range(member_count):
-            if response.json()['outfit_list'][0]['members'][i]['character_online_status']['online_status'] != "0":
+            if (
+                response.json()["outfit_list"][0]["members"][i][
+                    "character_online_status"
+                ]["online_status"]
+                != "0"
+            ):
                 count += 1
     except Exception as e:
         logging.exception(e)
