70cf89f63f21132cab0c8d1d81c52f6ef5f1478c
karlis-ozo <52657221+karlis-ozo@users.noreply.github.com>
2022-11-06 23:27:10 +0200

    Mostly ready. need to triple-check and add some doc-strings

--- src/commands/ops.py
+++ src/commands/ops.py
@@ -1,24 +1,25 @@
+import src.helpers.discord_checks as dc
 import urllib.parse
 import random
 import glob
 import disnake
 
-async def event_message(
-    inter,
-    message_body,
-    event
-):
 
+async def event_message(
+        inter,
+        message_body,
+        event
+):
     ops_dict = {
-    "Drill": ["Outfit Drill", "Planetside 2"],
-    "Casual": ["Casual Play", "Planetside 2"],
-    "FUAD": ["FUAD (Armoured Division)", "Planetside 2"],
-    "FUAF": ["FUAF (Air Force)", "Planetside 2"],
-    "FUBG": ["FUBG (Builders Group)", "Planetside 2"],
-    "FUEL": ["FUEL (Emerging Leaders)", "Planetside 2"],
-    "FUGG": ["FUGG (Galaxy Group)", "Planetside 2"],
-    "Huntsmen": ["Huntsmen", "Planetside 2"],
-    "ArmaOps": ["", "Arma 3"]
+        "Drill": ["Outfit Drill", "Planetside 2"],
+        "Casual": ["Casual Play", "Planetside 2"],
+        "FUAD": ["FUAD (Armoured Division)", "Planetside 2"],
+        "FUAF": ["FUAF (Air Force)", "Planetside 2"],
+        "FUBG": ["FUBG (Builders Group)", "Planetside 2"],
+        "FUEL": ["FUEL (Emerging Leaders)", "Planetside 2"],
+        "FUGG": ["FUGG (Galaxy Group)", "Planetside 2"],
+        "Huntsmen": ["Huntsmen", "Planetside 2"],
+        "ArmaOps": ["", "Arma 3"]
     }
     game = ops_dict[event][1]
     teamspeak_channel = ops_dict[event][0]
@@ -28,32 +29,32 @@ async def event_message(
         channel_name = "a3-announcements"
 
     # find the channel, where to send the message
-    channel = disnake.utils.get(inter.guild.text_channels, name = channel_name)
+    channel = disnake.utils.get(inter.guild.text_channels, name=channel_name)
 
     # find PS2 role, that should be Tagged:
-    role_to_ping = disnake.utils.get(inter.guild.roles, name = ops_dict[event][1])
-
+    role_to_ping = disnake.utils.get(inter.guild.roles, name=ops_dict[event][1])
 
     if channel is None or role_to_ping is None:
+        # todo: add tis
         await inter.edit_original_message("Impossible. Perhaps the Archives are incomplete." +
-                                          f"\n channel `{channel_name}` or role `{event}` doesn't exist")
-    elif not channel.permissions_for(channel.guild.me).send_messages:
-        await inter.edit_original_message("My lord, is that legal? \n I don't have the permissions to send there")
-    elif not channel.permissions_for(inter.author).send_messages:
-        await inter.edit_original_message(
-            "Imitating the Captain, huh? Surely that violates some kind of Starfleet protocol." +
-            "\n You don't have the permission to announce, so I won't"
-        )
+                                          f"\n Role `{event}` doesn't exist")
+    elif not await dc.channel_exists(inter, channel_name):
+        return
+    elif not await dc.check_if_bot_has_permission(inter, channel):
+        return
+    elif not await dc.user_or_role_has_permission(inter, channel=channel, can_write=True):
+        return
     else:
         try:
             teamspeak_channel.encode('utf-8')
             teamspeak_channel = urllib.parse.quote(str(game + "/" + teamspeak_channel), safe="")
             team_speak = disnake.ui.Button(style=disnake.ButtonStyle.url,
-                                           url="https://invite.teamspeak.com/ts.fugaming.org/?password=futs&channel="+str(teamspeak_channel),
+                                           url="https://invite.teamspeak.com/ts.fugaming.org/?password=futs&channel=" + str(
+                                               teamspeak_channel),
                                            label="Open TeamSpeak")
-            Message = await message_embed(message_body, game, event) #eval(str(event.lower()) + "(message_body)")
+            Message = await message_embed(message_body, game, event)  # eval(str(event.lower()) + "(message_body)")
             Message.set_image(
-                file=disnake.File(fp=random.choice(glob.glob("./assets/splash_art/"+str(event.lower())+"/*.png")))
+                file=disnake.File(fp=random.choice(glob.glob("./assets/splash_art/" + str(event.lower()) + "/*.png")))
             )
             await channel.send(role_to_ping.mention, embed=Message, components=team_speak,
                                delete_after=18000)
@@ -65,85 +66,86 @@ async def event_message(
 async def message_embed(message_body, game, event):
     title = str(game) + " - " + str(event)
     if "arma" in str.lower(game):
-        title += " - Starting ~~NOW~~ SOON (in 1 h or something)"
-        message_body = "IDK when, it's not like you allow a robot to play with you"
+        title = "Main Operation - Starting in 1 h"
     else:
         title += " - Starting NOW!"
     # Arma 3
     Message = disnake.Embed(
-            title=title,
-            color=0x9E0B0F,
-            description=str(message_body),
-            )
+        title=title,
+        color=0x9E0B0F,
+        description=str(message_body),
+    )
     await globals()[event.lower()](Message)
     return Message
 
 
 async def drill(Message):
     Message.add_field(
-            name="Join the conversation on TeamSpeak",
-            value="For chat, tactics and discussion.",
-            )
+        name="Join the conversation on TeamSpeak",
+        value="For chat, tactics and discussion.",
+    )
     return Message
 
 
 async def casual(Message):
     Message.add_field(
-            name="Join the conversation on TeamSpeak",
-            value="For chat, tactics and discussion.",
-            )
+        name="Join the conversation on TeamSpeak",
+        value="For chat, tactics and discussion.",
+    )
     return Message
 
 
 async def fugg(Message):
     Message.add_field(
-            name="Join the conversation on TeamSpeak",
-            value="For chat, tactics and discussion.",
-            )
+        name="Join the conversation on TeamSpeak",
+        value="For chat, tactics and discussion.",
+    )
     return Message
 
 
 async def fubg(Message):
     Message.add_field(
-            name="Join the conversation on TeamSpeak",
-            value="For relaxation, calm thoughts and stress free building.",
-            )
+        name="Join the conversation on TeamSpeak",
+        value="For relaxation, calm thoughts and stress free building.",
+    )
     return Message
 
 
 async def fuad(Message):
     Message.add_field(
-            name="Join the conversation on TeamSpeak",
-            value="For chat, tactics and discussion.",
-            )
+        name="Join the conversation on TeamSpeak",
+        value="For chat, tactics and discussion.",
+    )
     return Message
+
 
 async def fuaf(Message):
     Message.add_field(
-            name="Join the conversation on TeamSpeak",
-            value="For chat, tactics and discussion.",
-            )
+        name="Join the conversation on TeamSpeak",
+        value="For chat, tactics and discussion.",
+    )
     return Message
 
 
 async def huntsmen(Message):
     Message.add_field(
-            name="TeamSpeak is Mandatory!",
-            value="Hit the button below to go straight there.",
-            )
+        name="TeamSpeak is Mandatory!",
+        value="Hit the button below to go straight there.",
+    )
     return Message
 
 
 async def fuel(Message):
     Message.add_field(
-            name="Join the conversation on TeamSpeak",
-            value="For chat, tactics and discussion.",
-            )
+        name="Join the conversation on TeamSpeak",
+        value="For chat, tactics and discussion.",
+    )
     return Message
+
 
 async def armaops(Message):
     Message.add_field(
-            name="Join us on TeamSpeak?",
-            value="Am I a goodadmin now?  Oh, and also:\n Get your TFAR's ready!",
-            )
+        name="Join us on TeamSpeak?",
+        value="Get your TFAR's ready!",
+    )
     return Message
