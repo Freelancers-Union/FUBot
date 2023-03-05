0b83b8c03e3091fdcf348ed78346d59536ab8e0d
Normandabald <lpassingham@hotmail.co.uk>
2023-03-05 16:24:49 +0000

    Used S3 static images

--- src/send_intro.py
+++ src/send_intro.py
@@ -109,13 +109,14 @@ class SendIntro(commands.Cog):
 
     async def paginator(self):
         # Creates the embeds as a list.
+        s3_assets = "https://fu-static-assets.s3.eu-west-1.amazonaws.com/intro-icons/"
         embeds = [
           disnake.Embed(
                 title="Welcome to The Freelancers Union [FU]!",
                 description="This message is a quick start guide to help you get the most out of what FU has to offer.\nUse the buttons below to navigate through the pages.\n\nIf you have any questions, feel free to ask in the [#general](https://discord.com/channels/282514718445273089/282514718445273089) channel.",
                 colour=disnake.Colour(3092790),
             ).set_thumbnail(
-              url="https://cdn.discordapp.com/attachments/986678839008690176/1071460285170327572/01-logo.png"
+              url="https://fu-static-assets.s3.eu-west-1.amazonaws.com/splash_art/fu/fu-logo.png"
               ),
 
           disnake.Embed(
@@ -123,7 +124,7 @@ class SendIntro(commands.Cog):
               description="FU is mostly EU based, Prime Time for us is:\n<t:1669921200:t> - <t:1667340000:t>\n(Handily converted to your timezone!)\n\nWe run events almost everyday! Check the [#schedule](https://discord.com/channels/282514718445273089/539196000000000000/539196000000000000) channel for more info.",
               colour=disnake.Colour(4533298),
           ).set_thumbnail(
-            url="https://cdn.discordapp.com/attachments/986678839008690176/1071460285430366249/02-schedule.png"
+            url=f"{s3_assets}schedule.png"
           ),
 
           disnake.Embed(
@@ -131,7 +132,7 @@ class SendIntro(commands.Cog):
               description="FU is sub-divided based on the main games we play, but you'll find the same FU ethos and playstyle throughout.\n:white_small_square: **PlanetSide 2**\n:white_small_square: **Arma 3**",
               colour=disnake.Colour(6039085),
           ).set_thumbnail(
-            url="https://cdn.discordapp.com/attachments/986678839008690176/1071460285736558735/03-divs.png"
+            url=f"{s3_assets}divisions.png"
           ),
 
           disnake.Embed(
@@ -139,7 +140,7 @@ class SendIntro(commands.Cog):
               description="We use **Discord** as our main text forum, and **Teamspeak** for voice chat.",
               colour=disnake.Colour(7479593),
           ).set_thumbnail(
-            url="https://cdn.discordapp.com/attachments/986678839008690176/1071460286009200650/04-discord.png"
+            url=f"{s3_assets}communication.png"
             ).add_field(
               name="Discord",
               value="Discord is our main hub for chatting outside of games. Meet the community, grab roles for the games you want to play, and chat with fellow players in  the divisional channels:\n> [#role-menu](https://discordapp.com/channels/282514718445273089/983432774935531551)\n> [#ps2-general](https://discord.com/channels/282514718445273089/290929845788213249)\n> [#a3-general](https://discord.com/channels/282514718445273089/531220605391994892)\n** **",
@@ -155,7 +156,7 @@ class SendIntro(commands.Cog):
                 description="There are two main categories of leadership in FU: \n:white_small_square: **Community Leadership**\n:white_small_square: **Game Leadership**\n(*they are similar but not the same*)",
                 colour=disnake.Colour(8985637),
             ).set_thumbnail(
-                url="https://cdn.discordapp.com/attachments/986678839008690176/1071460286269227068/05-leadership.png"
+                url=f"{s3_assets}leadership.png"
             ),
 
           disnake.Embed(
@@ -163,7 +164,7 @@ class SendIntro(commands.Cog):
               description="*Leadership on a longer timespan*\n\n:white_small_square: **The Community**, with its sub-divisions, are led by **Officers**.\n:white_small_square: Officers cooperate to **create content** and **develop the community**. \n:white_small_square: An Officer contributes what is **within their reasonable limits**.\n:white_small_square: If you are interested to learn more about leadership contact Mordus#5149",
               colour=disnake.Colour(10425888),
           ).set_thumbnail(
-            url="https://cdn.discordapp.com/attachments/986678839008690176/1071460286269227068/05-leadership.png"
+            url=f"{s3_assets}leadership.png"
           ),
 
           disnake.Embed(
@@ -171,7 +172,7 @@ class SendIntro(commands.Cog):
               description="*Leadership on a short timespan*\n\n:white_small_square: **Game leadership** is **open to everyone**. It is not limited to Officers or even FU members.\n:white_small_square:  Whoever is the squad/platoon leader is the highest ranking in that context.\n:white_small_square: Your squad, your rules! \n:white_small_square:  The FU Ethos should always be followed.",
               colour=disnake.Colour(11866396),
           ).set_thumbnail(
-            url="https://cdn.discordapp.com/attachments/986678839008690176/1071460286269227068/05-leadership.png"
+            url=f"{s3_assets}leadership.png"
           ),
 
           disnake.Embed(
@@ -179,7 +180,7 @@ class SendIntro(commands.Cog):
                 description=">  It's just a game and the first rule of the game is to deny that it is a game because it is, in fact, not just a game.\n\n*The FU Ethos refers to implicitly held values on what is considered as good practise when playing together.*\n\nIn no specific order:\n\n:white_small_square: Welcome everyone! You are engaging with people, not numbers.\n:white_small_square: Do not use coercive methods to as a means to influence others.\n:white_small_square: It is voluntary to join an FU squad but if you do you commit to cooperate and follow the leaders instructions.\n:white_small_square: Our leadership ethos in FU is Service. A leader is there to provide the group and a direction for others to join.\n:white_small_square: Appreciate those who lead by the Ethos. Thank them for their effort when they step down. \n:white_small_square: Be mindful of how you speak of other players and groups. Maybe they cant hear you but we can.\n:white_small_square: Discuss problems but do not complain. Figure out improvements or ways to adapt.\nUse your initiative and step up when asked to volunteer.",
                 colour=disnake.Colour(13372183),
             ).set_thumbnail(
-                url="https://cdn.discordapp.com/attachments/986678839008690176/1071460286516699226/08-ethos.png"
+                url=f"{s3_assets}ethos.png"
             ),
 
           disnake.Embed(
@@ -187,7 +188,7 @@ class SendIntro(commands.Cog):
               description="*FU membership means identifying with the goals and values of the community. \nBecoming a member is **your choice**, not something we need you to become.\nYou can still play with us without being a member.*\n\nMembership gives you the member rank in PlanetSide, the **[FU]** tag on TS and exclusive access to Member only events and the Discord member's channels. \n\n**To become a member:**\n:white_small_square: (Optional but recommended) Read the [**Introduction**](https://wiki.fugaming.org/intro-module) document on our Wiki\n:white_small_square: Click the **Membership** button at the bottom of this message.\n\nIf you don't want membership right now you will be given Guest status on our discord and not notified again about introduction events. \nYou may at any time ask for membership should you change your mind.",
               colour=disnake.Colour(14812691),
           ).set_thumbnail(
-            url="https://cdn.discordapp.com/attachments/986678839008690176/1071460284922855444/09-membership.png"
+            url=f"{s3_assets}membership.png"
           ),
         ]
         return embeds
