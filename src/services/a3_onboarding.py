9b0941124c0eab12cd46d27dd1e3673691ff6ac4
Karlish <52657221+Karlish-git@users.noreply.github.com>
2024-03-22 09:14:35 +0200

    fix formatting

--- src/services/a3_onboarding.py
+++ src/services/a3_onboarding.py
@@ -1,9 +1,9 @@
-import time
 import logging
-import datetime
+from datetime import datetime
+
 import disnake
-from disnake.ext import commands
 import helpers.discord_checks as dc
+from disnake.ext import commands
 
 
 class OnboardA3Member(commands.Cog):
@@ -18,39 +18,51 @@ class OnboardA3Member(commands.Cog):
         self.accepted_id = "accepted"
         self.rejected_id = "rejected"
 
-    async def coog_load(self):
-        # create the onboarding category if it doesn't exist
-        for guild in self.bot.guilds:
-            arma_officer_role = disnake.utils.get(
-                guild.roles, name=self.a3_officer_role
+    async def cog_load(self):
+        try:
+            # create the onboarding category if it doesn't exist
+            logging.info("Creating onboarding category and notification channel")
+            for guild in self.bot.guilds:
+                arma_officer_role = disnake.utils.get(
+                    guild.roles, name=self.a3_officer_role
+                )
+                if not arma_officer_role:
+                    logging.warning(
+                        f"Role {self.a3_officer_role} not found in {guild.name}"
+                    )
+                    continue
+                if not disnake.utils.get(
+                        guild.categories, name=self.onboarding_category
+                ):
+                    overwrites = {
+                        arma_officer_role: disnake.PermissionOverwrite(
+                            view_channel=True,
+                            send_messages=True,
+                            manage_channels=True,
+                            manage_messages=True,
+                            read_message_history=True,
+                        ),
+                        guild.default_role: disnake.PermissionOverwrite(
+                            view_channel=False
+                        ),
+                    }
+                    await guild.create_category(
+                        self.onboarding_category,
+                        overwrites=overwrites,  # pyright: ignore
+                    )
+                if not disnake.utils.get(
+                        guild.channels, name=self.notification_channel
+                ):
+                    category = disnake.utils.get(
+                        guild.categories, name=self.onboarding_category
+                    )
+                    await guild.create_text_channel(
+                        name=self.notification_channel, category=category
+                    )
+        except Exception as e:
+            logging.exception(
+                "Error creating onboarding category and notification channel", e
             )
-            if not arma_officer_role:
-                logging.warning(
-                    f"Role {self.a3_officer_role} not found in {guild.name}"
-                )
-                continue
-            if not disnake.utils.get(guild.categories, name=self.onboarding_category):
-                overwrites = {
-                    arma_officer_role: disnake.PermissionOverwrite(
-                        view_channel=True,
-                        send_messages=True,
-                        manage_channels=True,
-                        manage_messages=True,
-                        read_message_history=True,
-                    ),
-                    guild.default_role: disnake.PermissionOverwrite(view_channel=False),
-                }
-                await guild.create_category(
-                    self.onboarding_category,
-                    overwrites=overwrites,  # pyright: ignore
-                )
-            if not disnake.utils.get(guild.channels, name=self.notification_channel):
-                category = disnake.utils.get(
-                    guild.categories, name=self.onboarding_category
-                )
-                await guild.create_text_channel(
-                    name=self.notification_channel, category=category
-                )
 
     @commands.Cog.listener(name="on_member_update")
     async def notify_officers(self, old: disnake.Member, new: disnake.Member):
@@ -58,32 +70,37 @@ class OnboardA3Member(commands.Cog):
         Send message to officers that new member has selected a3 role
         """
         if not (
-            disnake.utils.get(new.roles, name=self.a3_role)
-            and not disnake.utils.get(old.roles, name=self.a3_role)
+                disnake.utils.get(new.roles, name=self.a3_role)
+                and not disnake.utils.get(old.roles, name=self.a3_role)
         ):
             return
 
-        member_request = disnake.Embed(
-            title=f"New ArmA Player - {new.name}",
-            description=f"Contact newly enlisted {new.mention} to get them on battle field! :military_helmet:",
-            colour=disnake.Colour(14812691),
-        )
-
-        channel = disnake.utils.get(new.guild.channels, name=self.notification_channel)
-        if channel is not None and channel.type == disnake.ChannelType.text:
-            await channel.send(
-                embed=member_request,
-                components=[
-                    disnake.ui.Button(
-                        label="Assign to me",
-                        style=disnake.ButtonStyle.success,
-                        custom_id=self.onbarding_id
-                        + self.assign_id
-                        + "-"
-                        + str(new.id),
-                    )
-                ],
+        try:
+            member_request = disnake.Embed(
+                title=f"New ArmA Player - {new.name}",
+                description=f"Contact newly enlisted {new.mention} to get them on battle field! :military_helmet:",
+                colour=disnake.Colour(14812691),
             )
+
+            channel = disnake.utils.get(
+                new.guild.channels, name=self.notification_channel
+            )
+            if channel is not None and channel.type == disnake.ChannelType.text:
+                await channel.send(
+                    embed=member_request,
+                    components=[
+                        disnake.ui.Button(
+                            label="Assign to me",
+                            style=disnake.ButtonStyle.success,
+                            custom_id=self.onbarding_id
+                                      + self.assign_id
+                                      + "-"
+                                      + str(new.id),
+                        )
+                    ],
+                )
+        except Exception as e:
+            logging.exception("Error notifying officers", e)
 
     @commands.Cog.listener("on_button_click")
     async def onboard_listener(self, inter: disnake.MessageInteraction):
@@ -105,6 +122,7 @@ class OnboardA3Member(commands.Cog):
                 logging.info(
                     f"New member assigned to officer: {inter.component.custom_id}"
                 )
+            logging.info(f"Interacted with {inter.component.custom_id}")
 
             split = inter.component.custom_id.split("-")
             action = split[1]
@@ -144,9 +162,9 @@ class OnboardA3Member(commands.Cog):
                     overwrites=overwrites,
                     category=category,
                 )
-                for member in [member, inter.author]:
+                for user in [member, inter.author]:
                     await onboarding_channel.set_permissions(
-                        target=member,
+                        target=user,
                         view_channel=True,
                         send_messages=True,
                         read_message_history=True,
@@ -156,7 +174,12 @@ class OnboardA3Member(commands.Cog):
                 embed = inter.message.embeds[0]
                 embed.add_field(
                     name="Assigned To:", value=f"{inter.author.mention}", inline=False
-                ).set_footer(text=f"Communication initiated: <t:{time.time()}:F>")
+                ).set_footer(text=f"Communication initiated: {datetime.utcnow()} UTC")
+                embed.add_field(
+                    name="Onboarding Channel",
+                    value=f"{onboarding_channel.mention}",
+                    inline=False,
+                )
                 embed.colour = disnake.Colour(12757760)
                 await inter.response.edit_message(
                     embed=embed,
@@ -165,17 +188,17 @@ class OnboardA3Member(commands.Cog):
                             label="Accepted",
                             style=disnake.ButtonStyle.green,
                             custom_id=self.onbarding_id
-                            + self.accepted_id
-                            + "-"
-                            + str(member.id),
+                                      + self.accepted_id
+                                      + "-"
+                                      + str(member.id),
                         ),
                         disnake.ui.Button(
                             label="Rejected",
                             style=disnake.ButtonStyle.red,
                             custom_id=self.onbarding_id
-                            + self.rejected_id
-                            + "-"
-                            + str(member.id),
+                                      + self.rejected_id
+                                      + "-"
+                                      + str(member.id),
                         ),
                     ],
                 )
@@ -189,9 +212,9 @@ class OnboardA3Member(commands.Cog):
                 # Update the embed to show the new member has accepted and add the member role
                 embed.colour = disnake.Colour(1150720)
                 embed.add_field(
-                    name="Closed at:", value=f"<t:{time.time()}:F>", inline=False
+                    name="Closed at:", value=f"{datetime.utcnow()} UTC", inline=False
                 )
-                await inter.response.edit_message(embed=embed, components=None)
+
                 onboarding_channel = await dc.get_channel(
                     inter, channel_name=onbaoarding_channel_name
                 )
@@ -204,10 +227,12 @@ class OnboardA3Member(commands.Cog):
                 else:
                     await onboarding_channel.delete()
 
+                await inter.response.edit_message(embed=embed, components=None)
+
         except Exception as e:
             logging.exception(e)
             await inter.send(
-                content=f"Something went wrong, you should check the user's roles. and give Botanists this timestamp: {datetime.datetime.now()}",
+                content=f"Something went wrong, you should check the user's roles. and give Botanists this timestamp: {datetime.now()}",
             )
 
 
