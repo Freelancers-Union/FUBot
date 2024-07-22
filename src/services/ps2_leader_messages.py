491827ef9c8836ed4fb4668e9bdb1c4791a633f4
Karlish <52657221+Karlish-git@users.noreply.github.com>
2024-07-23 00:03:53 +0300

    fixed not reading channels

--- src/services/ps2_leader_messages.py
+++ src/services/ps2_leader_messages.py
@@ -28,7 +28,8 @@ class PS2LeaderBoaed(Cog):
         try:
             logging.info("Creating Leaderboard channel")
             for guild in self.bot.guilds:
-                if not disnake.utils.get(guild.channels, name=self.leaderbpard_channel):
+                channels = await guild.fetch_channels()
+                if self.leaderbpard_channel not in [channel.name for channel in channels]:
                     category = disnake.utils.get(
                         guild.categories, name=self.ps2_category
                     )
@@ -121,11 +122,11 @@ class PS2LeaderBoaed(Cog):
                 if leaderboard_message:
                     await leaderboard_message.edit(embeds=embeds)
                 else:
-                    await leaderboard_channel.send(embed=embeds)
+                    await leaderboard_channel.send(embeds=embeds)
 
         except Exception as exception:
             logging.error(
-                "Something went wrong logging ArmA3 server", exc_info=exception
+                "Something went wrong with ps2 LLeaderboard update", exc_info=exception
             )
 
 
